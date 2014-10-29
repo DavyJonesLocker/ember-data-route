69115e195459980dcf5e8d323b7575fb5636bb6e
Robert Jackson <robert.w.jackson@me.com>
2014-10-29 16:37:16 -0400

    Use resetController.
    This ensures that a `this.get('controller.model').rollback()` occurs
    even when transitioning within the same route.

--- addon/mixins/data-route.js
+++ addon/mixins/data-route.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
-  deactivate: function() {
+  resetController: function() {
     var model = this.get('controller.model');
     if(!model.get('isDeleted')) {
       model.rollback();
