a1bc2b006e7384226c107d33c5b510ccda9d21cc
Daniel Guettler <daniel.guettler@gmail.com>
2014-10-29 23:49:48 -0400

    Add organization with hasMany association to people

--- tests/dummy/app/routes/people/new.js
+++ tests/dummy/app/routes/people/new.js
@@ -3,6 +3,9 @@ import DataRouteMixin from 'ember-data-route/mixins/data-route';
 
 export default Ember.Route.extend(DataRouteMixin, {
   model: function() {
-    return this.store.createRecord('person');
+    var routeContext = this;
+    return this.store.find('organization', 1).then(function(organization) {
+      return routeContext.store.createRecord('person', { organization: organization });
+    });
   }
 });
